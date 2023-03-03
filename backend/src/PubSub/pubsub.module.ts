89963204dbce33e5eadb3e5896bd1784c7098368
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-02 23:33:19 -0300

    criando 2 tipos de pub/sub (Graphql)

--- backend/src/PubSub/pubsub.module.ts
+++ backend/src/PubSub/pubsub.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PubSub } from 'graphql-subscriptions';
 import { AuthorsResolver } from './Resolver/pubsub.resolver';
+import { connect } from 'mqtt';
+import { MQTTPubSub } from 'graphql-mqtt-subscriptions';
+
+const client = connect({
+  host: `${process.env.HOST_BROKER}`,
+  port: process.env.PORT_BROKER as unknown as number,
+  username: `${process.env.USER_BROKER}`,
+  password: `${process.env.PWD_BROKER}`,
+  reconnectPeriod: 1000,
+  protocol: `${process.env.PROTOCOL_BROKER}`,
+});
+
+const pubsub = new MQTTPubSub({
+  client,
+});
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [AuthorsResolver],
-  exports: [AuthorsResolver],
+  providers: [
+    AuthorsResolver,
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
+    },
+    {
+      provide: 'MQTT_PUB_SUB',
+      useValue: pubsub,
+    },
+  ],
+  exports: [],
 })
 export class PubSubModule {}
