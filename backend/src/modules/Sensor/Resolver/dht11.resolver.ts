fc458bd459a8932eb313d5d34d9b5c99d0786a96
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 14:19:10 -0300

    criando inserção de valores no postgres

--- backend/src/modules/Sensor/Resolver/dht11.resolver.ts
+++ backend/src/modules/Sensor/Resolver/dht11.resolver.ts
@@ -1,6 +1,10 @@
 import { Inject } from '@nestjs/common';
-import { Resolver, Subscription } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Subscription } from '@nestjs/graphql';
+import { Prisma } from '@prisma/client';
 import { MQTTPubSub } from 'graphql-mqtt-subscriptions';
+import { PrismaService } from 'nestjs-prisma';
+import { CreateOnedhtArgs } from 'src/graphql/dht/create-onedht.args';
+import { dht } from 'src/graphql/dht/dht.model';
 import { Dht11Dto } from '../Dto/dht11.dto';
 
 @Resolver((of) => Dht11Dto)
@@ -8,14 +12,22 @@ export class Dht11SensorResolver {
   constructor(
     @Inject('MQTT_PUB_SUB')
     private mqttpubSub: MQTTPubSub,
+    private prismaService: PrismaService,
   ) {}
 
   @Subscription((returns) => Dht11Dto, {
-    resolve(this: Dht11Dto, value) {
+    resolve(this: Dht11SensorResolver, value: Dht11Dto) {
+      this.createOneDataDht11({
+        data: { humidity: value.Humidity, temperature: value.Temperature },
+      });
       return value;
     },
   })
   getTempAndHumidity() {
-    return this.mqttpubSub.asyncIterator('DHT11/01');
+    return this.mqttpubSub.asyncIterator('SENSOR/ESP32/DHT11/01');
+  }
+  @Mutation((returns) => dht)
+  async createOneDataDht11(@Args() args?: CreateOnedhtArgs) {
+    return await this.prismaService.dht.create(args as Prisma.dhtCreateArgs);
   }
 }
