8499bc96ed7e20c3b4f02348445e4cf62b8720d9
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 14:18:07 -0300

    crianção resolver postgres e renoamento topicos

--- backend/src/modules/Actuator/Resolver/relay.resolver.ts
+++ backend/src/modules/Actuator/Resolver/relay.resolver.ts
@@ -1,32 +1,35 @@
 import { Inject } from '@nestjs/common';
-import {
-  Args,
-  Field,
-  Int,
-  Mutation,
-  ObjectType,
-  Query,
-  Resolver,
-  Subscription,
-} from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MQTTPubSub } from 'graphql-mqtt-subscriptions';
 import { RelayArgs, RelayDto } from '../dto/relay.dto';
+import { CreateOnerelayArgs } from 'src/graphql/relay/create-onerelay.args';
+import { relay } from 'src/graphql/relay/relay.model';
+import { PrismaService } from 'nestjs-prisma';
+import { Prisma } from '@prisma/client';
 
 @Resolver((of) => RelayDto)
 export class RelayActuatorResolver {
   constructor(
     @Inject('MQTT_PUB_SUB')
     private mqttpubSub: MQTTPubSub,
+    private prismaService: PrismaService,
   ) {}
 
   @Query((returns) => String)
   hello() {
     return 'Hello Word';
   }
-  @Mutation((returns) => RelayDto)
+  @Mutation((returns) => relay)
+  async createOneDataRelay(@Args() args?: CreateOnerelayArgs) {
+    return await this.prismaService.relay.create(
+      args as Prisma.relayCreateArgs,
+    );
+  }
+
+  @Mutation((returns) => relay)
   async switchStatus(@Args() args?: RelayArgs) {
-    this.mqttpubSub.publish('RELAY/01', args.OnOff);
-    return { OnOff: 1 };
+    this.mqttpubSub.publish('ACTUADOR/ESP32/RELAY/01', args.OnOff);
+    return this.createOneDataRelay({ data: { status_relay: 1 } });
   }
 
   // @Subscription((returns) => RelayActuator, {
