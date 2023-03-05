8477a54f566a3180727bea0329cfba9fb192dfba
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 14:20:36 -0300

    criação da estrutura do app(themes, routes, Apollo)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { ApolloProvider } from '@apollo/client'
+import { Button, CssBaseline, ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes'
+import { client } from './shared/services'
+import { DarkTheme } from './shared/themes'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={DarkTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 export default App
