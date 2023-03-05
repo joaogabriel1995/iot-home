2e761d15614665c70cc1a5da139bcd0e3e05c461
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 14:17:03 -0300

    adicionando modulo de storage no modulo actuator

--- backend/src/modules/Actuator/actuator.module.ts
+++ backend/src/modules/Actuator/actuator.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { PubSubModule } from 'src/modules/PubSub/pubsub.module';
+import { StorageModules } from '../Storage/storage.module';
 import { RelayActuatorResolver } from './Resolver/relay.resolver';
 @Module({
-  imports: [PubSubModule],
+  imports: [PubSubModule, StorageModules],
   controllers: [],
   providers: [RelayActuatorResolver],
 })
