ce47cb9c504ebaea1e8a904a4b908f3905a9ba11
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 14:18:39 -0300

    Add modulo storage no modulo de relay

--- backend/src/modules/Sensor/sensor.module.ts
+++ backend/src/modules/Sensor/sensor.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PubSubModule } from 'src/modules/PubSub/pubsub.module';
+import { StorageModules } from '../Storage/storage.module';
 import { Dht11SensorResolver } from './Resolver/dht11.resolver';
 @Module({
-  imports: [PubSubModule],
+  imports: [PubSubModule, StorageModules],
   controllers: [],
   providers: [Dht11SensorResolver],
 })
