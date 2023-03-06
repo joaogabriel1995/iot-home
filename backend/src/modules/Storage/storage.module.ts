12516c0cc4b12757af6baeee25e322c6bc8ea52c
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 22:57:11 -0300

    add em providers e exportando InfluxDbService

--- backend/src/modules/Storage/storage.module.ts
+++ backend/src/modules/Storage/storage.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PubSubModule } from 'src/modules/PubSub/pubsub.module';
 import { PrismaService } from 'nestjs-prisma';
+import { InfluxDbService } from './services/influxdb.service';
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, InfluxDbService],
+  exports: [PrismaService, InfluxDbService],
 })
 export class StorageModules {}
