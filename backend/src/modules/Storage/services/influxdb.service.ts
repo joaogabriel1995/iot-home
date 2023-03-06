9a95c123dbcbf80508ee0541c5c419f10127705d
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 22:58:19 -0300

    criando serviço comunicação com influx

---
+++ backend/src/modules/Storage/services/influxdb.service.ts
@@ -0,0 +1,61 @@
+import { InfluxDB } from '@influxdata/influxdb-client';
+import { Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
+@Injectable()
+export class InfluxDbService {
+  public connection: InfluxDB | null;
+  constructor() {
+    this.connection = null;
+    this.connect();
+  }
+  public connect(): void {
+    this.connection = new InfluxDB({
+      url: process.env.ENV_INFLUXDB_URL || '',
+      token: process.env.ENV_INFLUXDB_TOKEN || undefined,
+    });
+  }
+  public async getIdByName(): Promise<string> {
+    const response = await fetch(
+      `${process.env.ENV_INFLUXDB_URL}/api/v2/orgs?org=${process.env.ENV_INFLUXDB_ORG}`,
+      {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.ENV_INFLUXDB_TOKEN}`,
+        },
+      },
+    );
+    const data = await response.json();
+    const id: string = data.orgs[0].id;
+    return id;
+  }
+
+  public async getLastStatusMachine(
+    query: string,
+  ): Promise<string[] | undefined> {
+    if (this.connection) {
+      const idInflux = await this.getIdByName();
+      const queryApi = this.connection.getQueryApi(`${idInflux}`);
+
+      const fluxQuery = query;
+      const data: string[] = [];
+
+      return await new Promise((resolve, reject) => {
+        const fluxObserver = {
+          next(row, tableMeta) {
+            const result = tableMeta.toObject(row);
+            data.push(result);
+            console.log(result);
+            return result;
+          },
+          error: reject,
+          complete() {
+            resolve(data);
+          },
+        };
+
+        queryApi.queryRows(fluxQuery, fluxObserver);
+      });
+    }
+  }
+}
