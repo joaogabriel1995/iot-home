22f0630d784970687e5c9c12b5c2859b94834424
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-05 23:01:10 -0300

    update thema

--- frontend/src/shared/themes/Dark.ts
+++ frontend/src/shared/themes/Dark.ts
@@ -1,12 +1,19 @@
 import { createTheme } from '@mui/material/styles'
+import { typography } from '@mui/system'
 export const DarkTheme = createTheme({
   palette: {
     primary: {
       main: '#fff'
     },
+    secondary: {
+      main: '#000'
+    },
     background: {
       default: '#292738',
-      paper: '#fff'
+      paper: '#363447'
+    },
+    text: {
+      primary: '#fff'
     }
   }
 })
