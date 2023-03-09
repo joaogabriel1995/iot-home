fa6d8243ed5228928a6b57ff6ce6cee005f65960
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-08 21:24:27 -0300

    criando resolver para a aquisição de dados influx

--- backend/src/modules/Sensor/Resolver/dht11.resolver.ts
+++ backend/src/modules/Sensor/Resolver/dht11.resolver.ts
@@ -1,11 +1,13 @@
 import { Inject } from '@nestjs/common';
-import { Args, Mutation, Resolver, Subscription } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { MQTTPubSub } from 'graphql-mqtt-subscriptions';
 import { PrismaService } from 'nestjs-prisma';
 import { CreateOnedhtArgs } from 'src/graphql/dht/create-onedht.args';
 import { dht } from 'src/graphql/dht/dht.model';
-import { Dht11Dto } from '../Dto/dht11.dto';
+import { InfluxDbService } from 'src/modules/Storage/services/influxdb.service';
+import { dht11Args, Dht11Dto, influxDht11 } from '../Dto/dht11.dto';
+import { fluxgetMeanByTimeWindow } from '../Flux/sensor.flux';
 
 @Resolver((of) => Dht11Dto)
 export class Dht11SensorResolver {
@@ -13,6 +15,7 @@ export class Dht11SensorResolver {
     @Inject('MQTT_PUB_SUB')
     private mqttpubSub: MQTTPubSub,
     private prismaService: PrismaService,
+    private influxDbService: InfluxDbService,
   ) {}
 
   @Subscription((returns) => Dht11Dto, {
@@ -29,5 +32,17 @@ export class Dht11SensorResolver {
   @Mutation((returns) => dht)
   async createOneDataDht11(@Args() args?: CreateOnedhtArgs) {
     return await this.prismaService.dht.create(args as Prisma.dhtCreateArgs);
+  }
+  @Query((returns) => [influxDht11])
+  async getMeanByTimeWindow(@Args() args?: dht11Args) {
+    const a = await this.influxDbService.queryRows(
+      fluxgetMeanByTimeWindow(
+        args.timeRangeStartCalc,
+        args.windowPeriod,
+        args._field,
+      ),
+    );
+    console.log(a);
+    return a;
   }
 }
