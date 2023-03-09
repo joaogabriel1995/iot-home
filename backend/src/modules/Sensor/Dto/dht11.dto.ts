957e761c4ebbd0c48b5ccfd31e9339490614e00a
joaogabriel1995 <joaogabrielrp@gmail.com>
2023-03-08 21:21:09 -0300

    renomeando nome de funções

--- backend/src/modules/Sensor/Dto/dht11.dto.ts
+++ backend/src/modules/Sensor/Dto/dht11.dto.ts
@@ -1,4 +1,5 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ArgsType, Field, Int, ObjectType } from '@nestjs/graphql';
+import { influxModel } from 'src/modules/Storage/dto/influx.dto';
 
 @ObjectType()
 export class Dht11Dto {
@@ -6,4 +7,22 @@ export class Dht11Dto {
   Humidity: number;
   @Field(() => Number)
   Temperature: number;
+}
+
+@ObjectType()
+export class influxDht11 extends influxModel {
+  @Field(() => String)
+  sensor_ukey!: string;
+  @Field(() => String)
+  ssid!: string;
+}
+
+@ArgsType()
+export class dht11Args {
+  @Field(() => String)
+  timeRangeStartCalc!: string;
+  @Field(() => String)
+  _field!: string;
+  @Field(() => String)
+  windowPeriod!: string;
 }
